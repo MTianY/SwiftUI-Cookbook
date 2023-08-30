e0c8ea56e67027d7d4cf89cbc8aaf900fd5f76d7
TY <matianye@rayvision.com>
2023-08-30 11:13:16 +0800

    2

---
+++ SwiftUITest/SwiftUITest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by TY on 8/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Kodeco")
+            Text("Welcome to the Kodeco SwiftUI cookbook!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITest/SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by TY on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
